ffcefd75c56a6a53d8764bf2874596c529f70464
Leonardo Estevao <leooestevao@gmail.com>
2021-02-23 16:57:51 -0300

    Added ormConfigs Added migrations Separate express routes and server files

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
+// This needs to come first
+import 'reflect-metadata';
+// import './database'; It would also work, because the file that we want to import is named as 'index'
 import express from 'express';
+import './database/index.ts';
+
+import { router } from './routes';
+
 
 const app = express();
 
-
-app.get("/", (req, res) => {
-    return res.send("Rota Get Acessada com sucesso")
-})
-app.post('/', (req, res) => {
-    return res.send("Rota Post Acessada com sucesso")
-    
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => {
     console.log("Server ir running!")
