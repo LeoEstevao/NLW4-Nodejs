83aa7ca454a6d9459b10ccb0897e6dd1c0581edb
Leonardo Estevao <leooestevao@gmail.com>
2021-02-24 18:22:41 -0300

    Added surveys controller (create and show)

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import {UserController} from './controllers/UserController';
 
 const userController = new UserController();
+const surveyController = new SurveysController();
 const router = Router();
 
-router.post("/users", userController.create);
+router.post('/users', userController.create);
+router.post('/surveys', surveyController.create);
+
+router.get('/surveys', surveyController.show);
 
 export { router };
