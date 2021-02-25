fdc02efe93c8c1c168c1b096f8037553586bfb53
Leonardo Estevao <leooestevao@gmail.com>
2021-02-25 15:11:22 -0300

    Added SurveyUsers controllers (AS sendMailController) (Create)

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import { UserController } from './controllers/UserController';
 import { SurveysController } from './controllers/SurveysController';
-import {UserController} from './controllers/UserController';
+import { SendMailController } from './controllers/SendMailController'
 
 const userController = new UserController();
 const surveyController = new SurveysController();
+const sendMailController = new SendMailController();
 const router = Router();
 
 router.post('/users', userController.create);
 router.post('/surveys', surveyController.create);
+router.post('/sendMail', sendMailController.execute);
+
 
 router.get('/surveys', surveyController.show);
 
